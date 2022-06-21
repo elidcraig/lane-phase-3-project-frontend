2940cb2ca3688c1aff915353df61f0ea910a3957
Eli Craig <elidcraig@gmail.com>
2022-06-21 10:59:47 -0700

    Host passes vehicle state to cards

--- src/pages/Host.js
+++ src/pages/Host.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header.js'
 import HostCard from '../components/HostCard.js'
 import VehiclesContainer from '../components/VehiclesContainer.js'
@@ -6,15 +6,17 @@ import VehiclesContainer from '../components/VehiclesContainer.js'
 
 function Host() {
 
+  const [vehicles, setVehicles] = useState([])
+
   useEffect(() => {
     fetch(`http://localhost:9494/hosts/62`)
       .then(resp => resp.json())
-      .then(console.log)
+      .then(data => setVehicles(data.vehicles))
   }, [])
 
   return (
     <div className='host'>
-      This is the Host page.
+      <VehiclesContainer vehicles={vehicles}/>
     </div>
   );
 }
