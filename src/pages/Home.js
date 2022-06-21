d24d934321fc2d7d28eae4971c8346784dc997ca
Eli Craig <elidcraig@gmail.com>
2022-06-21 10:20:00 -0700

    Login component created, imports to Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from '../components/Header.js'
+import Login from '../components/Login.js'
 
 function Home() {
   return (
     <div className='home'>
+      <Header/>
       This is the home page.
     </div>
   );
