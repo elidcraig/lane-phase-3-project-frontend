e6f058f8e813f43249e465245ceecc5bf3c912ad
Eli Craig <elidcraig@gmail.com>
2022-06-22 08:56:33 -0700

    Stock image added to vehicle cards

--- src/components/VehicleCard.js
+++ src/components/VehicleCard.js
@@ -2,14 +2,16 @@ import React from 'react';
 import VehicleImage from './VehicleImage.js'
 import { Link } from 'react-router-dom'
 
-function VehicleCard({make, model, year, price_per_day, description}) {
+function VehicleCard({make, model, year, price_per_day, description, image_url}) {
   return (
     <div className='vehicle-card'>
-      <VehicleImage/>
+      <VehicleImage image_url={image_url}/>
       <h2>{year} {make} {model}</h2>
       <p>{description}</p>
       <h5>${price_per_day} / day</h5>
-      <Link to='/reservations/new' className='vehicle-card__link'>Book This Vehicle</Link>
+      <Link to='/reservations/new' className='vehicle-card__link'>
+        Book This Vehicle
+      </Link>
     </div>
   );
 }
--- src/components/VehicleImage.js
+++ src/components/VehicleImage.js
@@ -1,5 +1,9 @@
-function VehicleImage() {
-  return (<div></div>);
+function VehicleImage({image_url}) {
+  return (
+    <div className="vehicle-image">
+      <img alt='vehicle' src={image_url}/>
+    </div>
+  );
 }
 
 export default VehicleImage;
